b92cd0249b1367ecb16975551c530170f7ba7cbd
Markus Lewin <lewinmarkus@gmail.com>
2026-02-28 00:02:28 +0100

    Initial commit

---
+++ src/consts.ts
@@ -0,0 +1,37 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Saral Theme'
+export const SITE_DESCRIPTION =
+	'A simple theme for personal blog sites, created for Astro framework'
+
+// If you deploy to a subdirectory, set the `WEBSITE_BASE` here.
+// e.g. '/blog' if you are deploying to example.com/blog
+// Leave it as an empty string if you are deploying to a root domain (e.g. example.com)
+export const SITE_BASE = ''
+
+export const SocialLinks = [
+	{
+		name: 'GitHub',
+		url: 'https://github.com/markuslewin',
+	},
+	{
+		name: 'LinkedIn',
+		url: '#',
+	},
+]
+
+export const WebsiteLinks = [
+	{
+		name: 'Home',
+		url: '',
+	},
+	{
+		name: 'Blog',
+		url: 'blog',
+	},
+	{
+		name: 'About',
+		url: 'about',
+	},
+]
